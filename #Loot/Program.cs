897b9e35b025833df6aeed265cc40d2af0689477
De4dSh0t <ruipsilva2001@hotmail.com>
2019-10-17 22:18:26 +0100

    Loot

---
+++ #Loot/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _Loot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            
+            double rare = 0.10;
+            double uncommon = 0.30;
+            double common = 0.60;
+            float chance = r.Next(0.00, 1.00);
+
+        }
+    }
+}
