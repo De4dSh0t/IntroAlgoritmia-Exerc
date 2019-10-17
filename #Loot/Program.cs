38504703e8f9facc779fa67f89b8772982763d25
De4dSh0t <ruipsilva2001@hotmail.com>
2019-10-17 23:14:11 +0100

    Loot v1.1

--- #Loot/Program.cs
+++ #Loot/Program.cs
@@ -8,11 +8,28 @@ namespace _Loot
         {
             Random r = new Random();
             
-            double rare = 0.10;
-            double uncommon = 0.30;
-            double common = 0.60;
-            float chance = r.Next(0.00, 1.00);
+            string r1;
 
+            for(int i = 0; i < 2; i++)
+            {
+                int roll = r.Next(0,101);
+
+                if(roll <= 10)
+                {
+                    r1 = "a RARE";
+                    Console.WriteLine("You got " + r1 + " item!");
+                }
+                else if(roll > 10 && roll < 40)
+                {
+                    r1 = "an UNCOMMON";
+                    Console.WriteLine("You got " + r1 + " item!");
+                }
+                else if(roll > 40)
+                {
+                    r1 = "a COMMON";
+                    Console.WriteLine("You got " + r1 + " item!");
+                }
+            }
         }
     }
 }
