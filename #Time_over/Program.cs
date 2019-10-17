3fb96d32fa6b013ca6384052f211a338dfd22b27
De4dSh0t <ruipsilva2001@hotmail.com>
2019-10-17 22:32:06 +0100

    Time Over v1.0

---
+++ #Time_over/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _Time_over
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = 99;
+
+            for(int i = 0; i < 99; i++)
+            {
+                time -= 1;
+                Console.WriteLine(time);
+            }
+            if(time == 0)
+            {
+                Console.WriteLine("Time Over! Pretty fast hum...");
+            }
+        }
+    }
+}
