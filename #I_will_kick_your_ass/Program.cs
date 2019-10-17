77828a5d6af84fc22052966a3de6e9557bee7024
De4dSh0t <ruipsilva2001@hotmail.com>
2019-10-17 22:27:34 +0100

    IWKYA v1.1

--- #I_will_kick_your_ass/Program.cs
+++ #I_will_kick_your_ass/Program.cs
@@ -8,12 +8,14 @@ namespace _I_will_kick_your_ass
         {
             Random r = new Random();
 
-            int select = r.Next(1,3); //Saber quem começa o ataque (Player 1 ou Player 2)
+            //int select = r.Next(1,3); //Saber quem começa o ataque (Player 1 ou Player 2)
             //bool p1, p2; //Para ter o sitema "Tunn-Based attacks"
             int hp1 = r.Next(100,201); //HP do Player 1
             int hp2 = r.Next(100,201); //HP do Player 2
             int atk1 = r.Next(10,51); //Ataque do Player 1
             int atk2 = r.Next(10,51); //Ataque do Player 2
+            int ratio1 = hp1 / atk2; //Saber quantos turnos aguenta o Player1
+            int ratio2 = hp2 / atk1; //Saber quantos turnos aguenta o Player2
 
             //Saber quem ataca primeiro e Turn-Based Attacks (PERDA DE TEMPO!!!!!!!!!!!!!!!!!!!)
             //if(select == 1)
@@ -61,7 +63,7 @@ namespace _I_will_kick_your_ass
             //    }
             //}
 
-            if(hp1 > hp2 && atk1 > atk2)
+            if(ratio1 > ratio2)
             {
                 Console.WriteLine("Player 1 Wins!");
             }
