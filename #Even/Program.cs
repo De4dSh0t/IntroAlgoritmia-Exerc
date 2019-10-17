6b3b01ffe5730cee604041302db616d6310b52f9
De4dSh0t <ruipsilva2001@hotmail.com>
2019-10-17 22:34:35 +0100

    Even v1.0

---
+++ #Even/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _Even
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i <= 100; i +=2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
