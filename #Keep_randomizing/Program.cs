2b5b75f8e37c81f102b6c3d1ee957bac019c5af4
De4dSh0t <ruipsilva2001@hotmail.com>
2019-10-17 22:41:35 +0100

    KR V1.0

---
+++ #Keep_randomizing/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _Keep_randomizing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            int n = r.Next(1,10);
+            
+            Console.WriteLine("");
+            Console.WriteLine("The player moved " + n + " times!");
+            Console.WriteLine("");
+            Console.WriteLine("Positions:");
+            for(int i = 0; i < n; i++)
+            {
+                int pX = r.Next(1,100);
+                int pY = r.Next(1,100);
+                Console.WriteLine("(" + pX + ", " + pY + ")");
+            }
+        }
+    }
+}
