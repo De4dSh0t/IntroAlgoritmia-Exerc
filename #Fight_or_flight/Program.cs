e67e5467d64ee361ae2c6ceedcabbce9f953ca79
De4dSh0t <ruipsilva2001@hotmail.com>
2019-10-17 22:09:45 +0100

    Fight_or_flight

---
+++ #Fight_or_flight/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _Fight_or_flight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            int p1, p2; //Player1 e Player2
+            int dist; //Distância
+
+            p1 = r.Next(0,501); //Posição do Player1
+            p2 = r.Next(0,501); //Posição do Player2
+
+            dist = p1 - p2; //Distância entre o Player1 e o Player2
+
+            if(dist > -20 && dist < 20)
+            {
+                Console.WriteLine("Player 2 just flew away!");
+            }
+            else
+            {
+                Console.WriteLine("P1 and P2 are " + dist + " pixels away from each other...");
+            }
+        }
+    }
+}
