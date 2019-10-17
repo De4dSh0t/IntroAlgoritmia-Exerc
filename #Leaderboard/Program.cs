733684ffeb55a672e9d2fea1f651be5f35da4170
De4dSh0t <ruipsilva2001@hotmail.com>
2019-10-17 22:16:59 +0100

    Leaderboard

---
+++ #Leaderboard/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _Leaderboard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            int p1 = r.Next(1,10);
+            int p2 = r.Next(1,10);
+            int p3 = r.Next(1,10);
+
+            if(p1 == p2 || p1 == p3)
+            {
+                p1 = r.Next(1,10);
+            }
+            if(p2 == p3)
+            {
+                p2 = r.Next(1,10);
+            }
+
+            Console.WriteLine("::::::::::LeaderBoard::::::::::");
+            Console.WriteLine("          Player 1: " + p1);
+            Console.WriteLine("          Player 2: " + p2);
+            Console.WriteLine("          Player 3: " + p3);
+            Console.WriteLine(":::::::::::::::::::::::::::::::");
+        }
+    }
+}
