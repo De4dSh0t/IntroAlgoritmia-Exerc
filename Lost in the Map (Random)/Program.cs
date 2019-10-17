f377132c192381c8b77f404f10232d547c5530e1
De4dSh0t <ruipsilva2001@hotmail.com>
2019-10-17 22:19:32 +0100

    Lost in the Map

---
+++ Lost in the Map (Random)/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Lost_in_the_Map__Random_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            int x, y;
+            string l, r1, t, b;
+
+            x = r.Next(0,500);
+            y = r.Next(0,500);
+
+            //Posição X
+            if(x < 250)
+            {
+                l = " left";
+                r1 = "";
+            }
+            else
+            {
+                r1 = " right";
+                l = "";
+            }
+            
+            //Posição Y
+            if(y < 250)
+            {
+                t = " top";
+                b = "";
+            }
+            else
+            {
+                b = " bottom";
+                t = "";
+            }
+
+            Console.WriteLine("The Player is located at" + t + b + l + r1 + "!");
+        }
+    }
+}
