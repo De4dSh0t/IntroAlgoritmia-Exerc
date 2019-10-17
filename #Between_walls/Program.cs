91e74f80ecbec1ea7b4dafcd1978d9d6619140c9
De4dSh0t <ruipsilva2001@hotmail.com>
2019-10-17 22:45:56 +0100

    BW 1.0

---
+++ #Between_walls/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+
+namespace _Between_walls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            int n1 = r.Next(1,100);
+            int n2 = r.Next(1,100);
+            int max = n1;
+            int min = n2;
+
+            if(n2 > n1)
+            {
+                max = n2;
+                min = n1;
+            }
+            
+            for(int i = min; i < max; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
