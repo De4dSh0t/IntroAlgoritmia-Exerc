85f9a3603fb259e53b075ca569ea44da125e7073
De4dSh0t <ruipsilva2001@hotmail.com>
2019-10-26 16:04:21 +0100

    SN 1.0

---
+++ #Secret_number/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _Secret_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            int secret = r.Next(101);
+            bool flag = true;
+
+            while(flag)
+            {
+                int guess = r.Next(101);
+                
+                if(guess > secret)
+                {
+                    Console.WriteLine("Your guess is Higher than the secret number!");
+                }
+                else if(guess < secret)
+                {
+                    Console.WriteLine("Your guess is Smaller than the secret mumber.");
+                }
+                else
+                {
+                    Console.WriteLine("Congratulations! You've guessed it! The number was "+ secret + ".");
+                    flag = !flag;
+                }
+            }
+        }
+    }
+}
