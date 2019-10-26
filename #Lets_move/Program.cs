37e8388b9b352781b98b49dc82718acbdef91cad
De4dSh0t <ruipsilva2001@hotmail.com>
2019-10-26 15:54:42 +0100

    LM 1.0

---
+++ #Lets_move/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _Lets_move
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            int pX = 0;
+            int pY = 0;
+            int targetX = r.Next(1001);
+            int targetY = r.Next(1001);
+
+            for(int x = pX; x < targetX; x++)
+            {
+                pX ++;
+            }
+            for(int y = pY; y < targetY; y++)
+            {
+                pY ++;
+            }
+            Console.WriteLine("Player moved to (" + pX + "," + pY +").");
+        }
+    }
+}
