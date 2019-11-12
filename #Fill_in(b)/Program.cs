2de1164c5db00587f2b4d4c63e87edf5a0bd70ad
De4dSh0t <ruipsilva2001@hotmail.com>
2019-11-12 18:58:41 +0000

    B

---
+++ #Fill_in(b)/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _Fill_in_b_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] score = new int[10];
+
+            for(int i = 0; i < 10; i++)
+            {
+                score[i] = i + 1;
+                score[i+5] = (i + 1) * 10;
+            }
+        }
+    }
+}
