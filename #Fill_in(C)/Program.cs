83a7d01339d514b0b53b9ed42cab0b6267cff5aa
De4dSh0t <ruipsilva2001@hotmail.com>
2019-11-12 19:04:33 +0000

    C

---
+++ #Fill_in(C)/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _Fill_in_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] score = new int[11];
+            int a = 0;
+            int b = 1;
+
+            for(int i = 0; i < 11; i++)
+            {
+                score[i] = a;
+                a += b;
+                b += 2;
+                Console.WriteLine(score[i]);
+            }
+        }
+    }
+}
