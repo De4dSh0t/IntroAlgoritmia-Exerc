ef42217f610d5b94c180e25e97c9b547af7d3ba6
De4dSh0t <ruipsilva2001@hotmail.com>
2019-11-17 20:04:21 +0000

    Temp 1.0

---
+++ #Temperatures/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _Temperatures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            
+            int[] temps = new int[100];
+            int soma, media;
+
+            for(int i = 0; i < temps.Length; i++)
+            {
+                temps[i] = r.Next(-30, 51);
+
+                if(temps[i] < 0)
+                {
+                    Console.WriteLine(temps[i]);
+                }
+                
+                soma += temps[i];
+            }
+
+            media = soma / temps.Length;
+
+            Console.WriteLine("The average temperature is " + media);
+        }
+    }
+}
