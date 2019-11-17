5ba646d01b0951a5a61a797df088f0a12d52e7f8
De4dSh0t <ruipsilva2001@hotmail.com>
2019-11-17 19:57:12 +0000

    R 1.0

---
+++ #Raid/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _Raid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            string[] classes = new string[]{"Priest", "Mage", "Warrior", "Bandit", "Archer", "Necromancer"};
+            int[] nClasses = new int[6];
+
+            for(int i = 0; i < 20; i++)
+            {
+                int a = r.Next(0, 6);
+                nClasses[a]++;
+            }
+
+            int n = r.Next(0, 6);
+            Console.WriteLine(nClasses[n] + " " + classes[n]);
+        }
+    }
+}
