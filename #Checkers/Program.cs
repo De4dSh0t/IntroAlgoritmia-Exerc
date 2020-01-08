9b0486ba7d1b92d2858bfac2cda7e7ec62c44e8e
De4dSh0t <ruipsilva2001@hotmail.com>
2020-01-08 19:23:37 +0000

    Checkers 1.1

--- #Checkers/Program.cs
+++ #Checkers/Program.cs
@@ -11,6 +11,7 @@ namespace _Checkers
             string[,] board = new string[rows,columns];
 
             BoardFill(board, rows, columns);
+            PrintMatrix(BoardFill(board, rows, columns));
         }
 
         static string[,] BoardFill(string[,] board, int rows, int columns)
@@ -27,26 +28,33 @@ namespace _Checkers
                     if(a % 2 != 0) //Se "a" for ímpar
                     {
                         board[y, x] = "W"; //White
-                        if(a < 4 && board[y, x] == "W")
-                        {
-                            board[y,x] = "WhiteChecker";
-                        }
                     }
                     else //Se "a" for par
                     {
                         board[y, x] = "B"; //Black
-                        if(a > 5 && board[y, x] == "B")
-                        {
-                            board[y, x] = "BlackChecker";
-                        }
+                        if(b < 4) board[y,x] = "WhiteC";
+                        else if(b > 5) board[y,x] = "BlackC";
                     }
                     a++;
                 }
-
                 b++;
             }
 
             return board;
+        }
+
+        static void PrintMatrix(string[,] m)
+        {
+            for (int row = 0; row < m.GetLength(0); row++)
+            {
+                for (int col = 0; col < m.GetLength(1); col++)
+                {
+
+                    Console.Write(m[row, col]);
+                    Console.Write("\t");
+                }
+                Console.Write("\n");
+            }
         }
     }
 }
