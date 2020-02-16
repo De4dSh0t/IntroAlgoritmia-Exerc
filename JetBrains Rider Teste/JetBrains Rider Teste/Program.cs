7374f03068dff86b70653338a008bd1de8a2895e
De4dSh0t <ruipsilva2001@hotmail.com>
2020-02-16 22:35:46 +0000

    Teste

---
+++ JetBrains Rider Teste/JetBrains Rider Teste/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JetBrains_Rider_Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
