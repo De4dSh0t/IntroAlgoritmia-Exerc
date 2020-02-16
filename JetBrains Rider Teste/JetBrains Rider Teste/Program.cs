65f8beb1bacb9f550ea527df3679793e177a4ae7
De4dSh0t <ruipsilva2001@hotmail.com>
2020-02-16 23:29:00 +0000

    Second Commit

--- JetBrains Rider Teste/JetBrains Rider Teste/Program.cs
+++ JetBrains Rider Teste/JetBrains Rider Teste/Program.cs
@@ -6,7 +6,35 @@ namespace JetBrains_Rider_Teste
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int rows = 4;
+            int cols = 6;
+            int[,] matrix = new int[rows, cols];
+            Fill(matrix, rows, cols);
+            PrintMatrix(matrix, rows, cols);
+        }
+
+        static void Fill(int[,] matrix, int rows, int cols)
+        {
+            Random r = new Random();
+            for (int y = 0; y < rows; y++)
+            {
+                for (int x = 0; x < cols; x++)
+                {
+                    matrix[y, x] = r.Next(100);
+                }
+            }
+        }
+
+        static void PrintMatrix(int[,] matrix, int rows, int cols)
+        {
+            for (int y = 0; y < rows; y++)
+            {
+                for (int x = 0; x < cols; x++)
+                {
+                    Console.WriteLine(matrix[y,x]);
+                }
+                Console.WriteLine("\n");
+            }
         }
     }
 }
